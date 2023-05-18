f20d943253b634c333c8308e3e098febd64172b7
ryanafoster3 <133047745+ryanafoster3@users.noreply.github.com>
2023-05-18 12:37:16 -0500

    Update convert_img.py
    Added log file

--- convert_img.py
+++ convert_img.py
@@ -7,23 +7,25 @@ from PIL import Image
 input_dir = 'images/'
 out_dir = '/opt/icons/'
 
-# determine image size, format, and rotation direction
+# determine image size (size), format (ext), rotation direction
+# for files (rotation), and starting with a desired name format
 size = 128, 128
-ext = 'JPEG'
+ext = '.JPEG'
 rotation = -90
+edit_only = 'ic_'
 
 # iterate over the files from input directory
 for filename in os.listdir(input_dir):
-    # ensures item is a file and that files starting
-    # with 'ic_' are edited
+    # ensures item is a file and that only the files
+    # starting with 'ic_' are edited
     f = os.path.join(input_dir, filename)
-    if os.path.isfile(f) and filename.startswith('ic_'):
+    if os.path.isfile(f) and filename.startswith(edit_only):
         # creates output directory path and new file name
         # opens image, rotates (rotation), resizes (size), and saves
         # as a desired file type (ext)
-        n = os.path.join(out_dir, filename)
         im = Image.open(f)
-        im.rotate(rotation).resize(size).convert('RGB').save(n, ext)
+        n = os.path.join(out_dir, str(filename + ext))
+        im.rotate(rotation).resize(size).convert('RGB').save(n)
 
         # reports where the new image is stored as well as
         # displaying the new format and image size
@@ -33,3 +35,9 @@ for filename in os.listdir(input_dir):
         print("Processed image: " + f)
         print('New image saved as: ' + n)
         print('Format: ' + img_format + "  //  Size: " + img_size)
+        
+        # prints output to a log, uncomment to use
+        #with open("covert.log", "a") as f:
+        #    print("Processed image: " + f, file = f)
+        #    print('New image saved as: ' + n, file = f)
+        #    print('Format: ' + img_format + "  //  Size: " + img_size, file = f)
